6632c4e9ee54437b4ce18e07a5ae1c8e2c71e10c
“Jordan <jdouthit@westmont.edu>
2020-03-15 18:01:25 -0700

    Chat function working!

--- public/js/main.js
+++ public/js/main.js
@@ -19,12 +19,48 @@ if('undefined' == typeof username || !username){
     username = 'Anonymous_'+Math.random();
 }
 
-$( '#messages').append('<h4>'+username+'</h4>');
-
 /* connect to the socket server */
+var chat_room = 'One_Room';
 
 var socket = io.connect();
 
 socket.on('log', function(array){
-    console.log.apply(console,array);
-})
+    console.log.apply(console, array);
+});
+
+socket.on('join_room_response' ,function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    $( '#messages' ).append('<h4>New User joined the chat :  '+payload.username+ '</h4>');
+});
+
+socket.on('send_message_response' ,function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    console.log(' *** check to see if this is showing up for \'send_message_response\' or not');
+    $( '#messages' ).append('<p><b>'+payload.username+'    says:</b>  '+payload.message+'</p>');
+});
+
+function send_message(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+    payload.message = $('#send_message_holder').val();
+    console.log(' *** client log message: \'send_message\' payload:   '+JSON.stringify(payload));
+    socket.emit('send_message', payload);
+}
+
+
+$(function(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+
+    console.log(' * * * Client Log message: \'join_room\' payload:  ' +JSON.stringify(payload));
+    socket.emit('join_room' ,payload);
+
+});
